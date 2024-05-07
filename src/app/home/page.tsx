6001206df85a83ddc053dd99fc916025cc0628f5
Multyp <miguel.dasilva2004@hotmail.com>
2024-05-07 17:01:57 -0400

    edit: updated user dropdown

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -48,19 +48,17 @@ const Home: React.FC = () => {
     return (
       <div className="min-h-screen flex flex-row">
         {/* Hamburger menu content */}
+        {/* Sidebar for small screens */}
         <div className={`bg-gray-600 h-screen w-64 absolute top-0 left-0 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
           <div className="ml-2 h-12 w-12 absolute items-center justify-center cursor-pointer" onClick={closeMenu}>
             <FontAwesomeIcon icon={faTimes} className="h-6 w-6 text-white" />
           </div>
-          {/* User section */}
           <div className="flex flex-col justify-end h-full">
-            {/* Contacts section */}
             <div className="p-4 flex-grow">
               Contacts
             </div>
-            {/* User section */}
+            {/* User section for mobile */}
             <div className="p-4 flex items-center justify-between">
-              {/* User's card with image and username */}
               <div className="relative w-full">
                 <div className="flex items-center space-x-2 cursor-pointer hover:bg-gray-500 w-full rounded-md p-2" onClick={toggleDropdown}>
                   <img src={`data:image/svg+xml;base64,${currentUserImage}`} alt="User Avatar" className="w-10 h-10 rounded-full" />
@@ -68,23 +66,29 @@ const Home: React.FC = () => {
                 </div>
                 {/* Dropdown menu */}
                 {isDropdownOpen && (
-                  <div className="absolute right-0 top-auto bottom-full mt-2 w-48 bg-gray-600 rounded-md shadow-lg">
-                    <div className="py-1">
+                  <div className="absolute right-0 top-auto bottom-full w-full bg-gray-600 border border-gray-500 border-opacity-80 rounded-md shadow-lg">
+                    <div className="py-1 justify-center">
                       {/* Email section */}
                       <div className="px-4 py-2 flex items-center justify-between">
                         <span className="text-white">{currentUserMail}</span>
                       </div>
                       {/* Divider */}
-                      <div className="border-t border-gray-700"></div>
+                      <div className="flex w-full justify-center items-center">
+                        <div className="border-t border-gray-500 w-11/12"/>
+                      </div>
                       {/* Settings section */}
-                      <div className="px-4 py-2 flex items-center justify-between">
+                      <div className="px-4 py-2 flex items-center">
+                        <FontAwesomeIcon icon={faCog} className="h-5 w-5 text-white cursor-pointer mr-2" />
                         <span className="text-white">Settings</span>
-                        <FontAwesomeIcon icon={faCog} className="h-5 w-5 text-white cursor-pointer" />
+                      </div>
+                      {/* Divider */}
+                      <div className="flex w-full justify-center items-center">
+                        <div className="border-t border-gray-500 w-11/12"/>
                       </div>
                       {/* Logout section */}
-                      <div className="px-4 py-2 flex items-center justify-between">
+                      <div className="px-4 py-2 flex items-center">
+                        <FontAwesomeIcon icon={faSignOutAlt} className="h-5 w-5 text-white cursor-pointer mr-2" />
                         <span className="text-white">Logout</span>
-                        <FontAwesomeIcon icon={faSignOutAlt} className="h-5 w-5 text-white cursor-pointer" />
                       </div>
                     </div>
                   </div>
@@ -93,20 +97,53 @@ const Home: React.FC = () => {
             </div>
           </div>
         </div>
-        {/* Sidebar */}
-        <div className="hidden md:block bg-gray-600 h-screen w-64">
+        {/* Sidebar for large screens */}
+        <div className="hidden md:flex flex-col justify-end bg-gray-900 h-screen w-64">
           {/* Contacts section */}
-          <div className="p-4">Contacts</div>
-          {/* User section */}
-          <div className="p-4 cursor-pointer">User</div>
+          <div className="p-4 flex-grow">Contacts</div>
+          {/* User section for large screens */}
+          <div className="p-4 flex items-center justify-between">
+            <div className="relative w-full">
+              <div className="flex items-center space-x-2 cursor-pointer hover:bg-gray-500 w-full rounded-md p-2" onClick={toggleDropdown}>
+                <img src={`data:image/svg+xml;base64,${currentUserImage}`} alt="User Avatar" className="w-10 h-10 rounded-full" />
+                <span className="text-white">{currentUserName}</span>
+              </div>
+              {/* Dropdown menu */}
+              {isDropdownOpen && (
+                <div className="absolute right-0 top-auto bottom-full mt-2 w-full bg-gray-600 border border-gray-500 border-opacity-80 rounded-md shadow-lg">
+                  <div className="py-1 justify-center">
+                    {/* Email section */}
+                    <div className="px-4 py-2 flex items-center justify-between">
+                      <span className="text-white">{currentUserMail}</span>
+                    </div>
+                    {/* Divider */}
+                    <div className="flex w-full justify-center items-center">
+                      <div className="border-t border-gray-500 w-11/12"/>
+                    </div>
+                    {/* Settings section */}
+                    <div className="px-4 py-2 flex items-center">
+                      <FontAwesomeIcon icon={faCog} className="h-5 w-5 text-white cursor-pointer mr-2" />
+                      <span className="text-white">Settings</span>
+                    </div>
+                    {/* Divider */}
+                    <div className="flex w-full justify-center items-center">
+                      <div className="border-t border-gray-500 w-11/12"/>
+                    </div>
+                    {/* Logout section */}
+                    <div className="px-4 py-2 flex items-center">
+                      <FontAwesomeIcon icon={faSignOutAlt} className="h-5 w-5 text-white cursor-pointer mr-2" />
+                      <span className="text-white">Logout</span>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
         {/* Main content area */}
-        <div className="flex-1 bg-gray-800 w-full">
+        <div className="flex-1 bg-gray-700 w-full">
           {/* Chat container section */}
-          <div
-            className="h-16 w-16 flex items-center justify-center cursor-pointer md:hidden"
-            onClick={toggleMenu}
-          >
+          <div className="h-16 w-16 flex items-center justify-center cursor-pointer md:hidden" onClick={toggleMenu}>
             <FontAwesomeIcon icon={faBars} className="h-6 w-6 text-white" />
           </div>
           <div className="p-4 text-white">Chat Container</div>
